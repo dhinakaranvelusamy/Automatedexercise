3eae1b53cea8d7adb8fdd0e7f5040e29a7cb0930
Vignesh.E <eswaranevignesh@gmail.com>
2025-09-18 14:35:18 +0530

    Add project files.

---
+++ Automatedexercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using AutomatedExercise;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Automatedexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailSender obj = new EmailSender();
+            obj.SendEmail();
+
+            businessLayer obj = new businessLayer();
+
+
+        }
+    }
+}
---
+++ MailExercise/EmailExercuse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MailExercise
+{
+    public abstract class AbstractEmailClass
+    {
+
+        public string Emailadress;
+        public string Toadress;
+        public string Subject;
+        public string Content;
+
+        public AbstractEmailClass(string Email, string Toadress)
+        {
+            this.Emailadress = Email;
+            this.Toadress = Toadress;
+        }
+        public void Fromaddress()
+        {
+            Console.WriteLine("enter the From email Id");
+           Emailadress= Console.ReadLine();
+        }
+        public void GetToAddress()
+        
+        {
+            Console.WriteLine("Enyter To address");
+            Toadress = Console.ReadLine();
+        }
+
+        public virtual void GetSubject()
+        {
+            Console.WriteLine("Enter the  subject");
+            Subject =Console.ReadLine();
+        }
+
+        public virtual void GetContent()
+        {
+            Console.WriteLine("EnyterFrom Address");
+            Content = Console.ReadLine();
+        }
+
+
+        public static void ISValidEmailAdress(string email)
+        { }
+        public abstract void SendEmail(string subject, string content);
+
+    }
+}
+
+
+
+    
